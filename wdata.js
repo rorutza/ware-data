db173a534db08667c432bb17a93c7d3905e13778
Diana Kramer <diana@warelet.com>
2013-07-08 14:15:09 +0200

    Name changed

--- wdata.js
+++ wdata.js
@@ -1,50 +1,59 @@
 $(document).ready(function () {
 
-    var d = $("#microudata");
+var udata = {
+    article: {
+        name: "",
+        about: "",
+        version: "",
+        comment: "",
+        audio: "",
+        video: "",
+        date: ""
+        },
+    author: {
+        name: "",
+        email: "",
+        telephone: "",
+        url: ""
+    }
+};
 
-    /*    $("#buton1").click(function () {
-     $("#about").html($("#wabout").val());
-     });
-     $("#buton2").click(function () {
-     $("#accountablePerson").html($("#waccountablePerson").val());
-     });
-     */
-    /*    $("#buton3").click(function () {
-     var wval = $("#waudio").val();
-     d.append('<span id = "new" itemprop = "audio" content = "About">');
-     d.append(wval);
-     d.append('</span>');
-     });
-     */
-    var udata = {
-        uprop: "test",
-        uvalue: "test"
-    };
-//TODO - other ways of doing it
-    $("input").change(function () {
-        udata.uprop = this.name;
-        udata.uvalue = this.value;
-        if (console.log) {
-            console.log(udata.uprop + ' ' + udata.uvalue);
-        }
-        var dl = $("#properties");
-        var dt = dl.append($("<dt>" + udata.uprop + "</dt>"));
-        //var dt = $("#" + udata.uprop);
-        var dd = dt.next();
-        if (dd.is("dd")) {
-            dd.remove();
-        }
-        dt.append($("<dd itemprop=\"" + udata.uprop + "\">" + udata.uvalue + "</dd>"));
-        //   var a = obtenir(udate.uid);
-    });
 
-    /*
-     $("#buton4").click(function () {
+//Create object with a closure
+//Known object - author
+//itemtype = author;
 
-     });
-     */
-    /*
-     *   <input id = "wabout" name = "wabout" type = "text" value = ""/>
-     *   <span id = "about" itemprop = "about" content = "About"></span>
-     */
+$("input").change(function () {
+    var itemtype = this.className;
+    var itemprop = this.name;
+    udata[itemtype][itemprop] = this.value;
+    //   var itemprop = this.name;
+    if (console.log) {
+        console.log('itemtype = ' + itemtype + ' --- ');
+        console.log('itemprop = ' + itemprop + ' --- ');
+        console.log('udata = ' + udata[itemtype][itemprop]);
+        //console.log('meta.itemtype.itemprop = ' + meta.itemtype.itemprop);
+    }
+/*
+    var dl = $("#properties");
+    var dt = dl.append($("<dt>" + itemprop + "</dt>"));
+ //var dt = $("#" + udata.uprop);
+    var dd = dt.next();
+    if (dd.is("dd")) {
+        dd.remove();
+    }
+ dt.append($("<dd itemprop=\"" + itemprop + "\">" + print + "</dd>"));
+ //   var a = obtenir(udate.uid);
+ */
+});
+
+ /*
+ $("#buton4").click(function () {
+
+ });
+ */
+/*
+ *   <input id = "wabout" name = "wabout" type = "text" value = ""/>
+ *   <span id = "about" itemprop = "about" content = "About"></span>
+ */
 });
