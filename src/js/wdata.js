9192666666501ed58749e4ef58991da5c5c1ec2b
Diana Kramer <diana@warelet.com>
2013-07-15 05:02:54 +0200

    Fieldset separation in form.

--- src/js/wdata.js
+++ src/js/wdata.js
@@ -2,18 +2,17 @@ $(document).ready(function () {
 
     //Get selected schema
     //debbug -- selected_schema: "Article"
-    //testType(schemas[selected_schema]); -- Type of [object Object] is object! Is Array: false
 
     var form = $("form");
 
     form.on("change", "select#selectschema", function() {
-        var schema_input = $("#schemainput");
+        //var schema_input = $("#schemainput");
         var selected_schema = $(this).val();
         console.log("Selected Schema: " + selected_schema);
         //printSchema(selected_schema);
-        schema_input.children().remove();
+        $('fieldset:nth-child(2)').remove();
         if ("none" != selected_schema) {
-            printForm.call(schema_input, selected_schema);
+            printForm.call(form, selected_schema);
         }
     });
 
@@ -21,47 +20,49 @@ $(document).ready(function () {
     //printForm
     //Array - multiple options
     //not(Array) - input
-    //<label>K:</label> <input class = PARAM name = K type = V/><br>
-    //name - K - propiedad
-    //type - V - type
+    //<label>prop:</label> <input class = PARAM name = prop type = type/><br>
+    //name - prop - propiedad
+    //type - type - type
     //class - PARAM - schema/object
 
     //printForm()param
     //param - selected Schema
-    //k - property - itemprop
-    //v - type - itemtype if type object
-    //v[] - Schema if type object
+    //prop - property - itemprop
+    //type - type - itemtype if type object
+    //type[] - Schema if type object
+
+    //testType(schemas[selected_schema]); -- Type of [object Object] is object! Is Array: false
+
 
     function printForm(param) {
-        var schema_input = $(this);
+        $(this).append('<fieldset></fieldset>');
+        var schema_input = $("fieldset:empty");
         printInfo.call(schema_input, param);
         var s = schemas[param]; // s - [object Object]
         console.log("###printForm - schema:" + param);
-        for(var k in s) {
-            var v = s[k];
-            console.log("type = v = s[k] = " + v)
-            if(v instanceof Array) {
-                printInfo.call(schema_input, k);
-                console.log("k - " + k + ": [");
-                for(var j = 0; j < v.length; ++j) {
+        for(var prop in s) {
+            var type = s[prop];
+            console.log("type = s[prop] = " + type)
+            if(type instanceof Array) {
+                printInfo.call(schema_input, prop);
+                console.log("prop - " + prop + ": [");
+                for(var j = 0; j < type.length; ++j) {
                     console.log("j - " + j);
-                    console.log("v[j] - " + v[j] + ": {");
-                    printRadio.call(schema_input, param,v[j],k);
+                    console.log("type[j] - " + type[j] + ": {");
+                    printRadio.call(schema_input, param, type[j], prop);
                     console.log("}");
                 }
-                printRadio.call(schema_input, param,"text",k);
+                printRadio.call(schema_input, param, "text", prop);
                 console.log("]");
             } else {
-                console.log("k - " + k + ": " + "v - " + v);
-                printInput.call(schema_input, param, k, v);
+                console.log("prop - " + prop + ": " + "type - " + type);
+                printInput.call(schema_input, param, prop, type);
             }
         }
-
         //prop, type, k, v can be used directly in form print
         //problems with "text" - remembers last value
-
-
     };
+
     function printInput(param, prop, type) {
         var inputText = '<div><label>' + prop + '</label>' +
             '<input class=\"' + param +
@@ -71,10 +72,10 @@ $(document).ready(function () {
         $(this).append(inputText);
     };
 
-    function printRadio(param, prop, radio) { //param-schema, prop-itemprop. radio-value
-        var inputRadio = '<div><label>- ' + prop + '</label><input class=\"' +
-            param + '\" name=\"' + radio + '\" value=\"' +
-            prop + '\" type=\"radio\"></input></div>';
+    function printRadio(param, type, prop) { //param-schema, type-itemtype, prop-itemprop
+        var inputRadio = '<div><label>- ' + type + '</label><input class=\"' +
+            param + '\" name=\"' + prop + '\" value=\"' +
+            type + '\" type=\"radio\"></input></div>';
         console.log("###inputRadio: " + inputRadio);
         $(this).children(":last-child").append(inputRadio);
     };
@@ -87,23 +88,14 @@ $(document).ready(function () {
 
     form.on("change", "input[type='radio']", function() {
         var selected_radio = $(this).val(); //Selected schema
-
         console.log("###Selected Radio: " + selected_radio);
         if ("text" != selected_radio) {
-            printForm.call($(this).parent(), selected_radio);
+            printForm.call($(this).parent().parent(), selected_radio);
         } else {
             console.log("###className of this when text = " + this.className);
-            printInput.call($(this).parent(), this.className, this.name, this.value);
+            printInput.call($(this).parent().parent(), this.className, this.name, this.value);
         }
     });
-    /*        function fragmento(html) {
-     var inputFrag = $(document.createDocumentFragment());
-     console.log("inputFrag: " + inputFrag);
-     var newFrag = $(inputFrag).append(html);
-     console.log("newFrag: " + newFrag);
-     return newFrag;
-     };
-     */
 
     /*
     udata - schema
@@ -131,26 +123,7 @@ $(document).ready(function () {
 
     var theclicks = new Myclicks();
 
-    // dt.append($("<dd itemprop=\"" + itemprop + "\">" + print + "</dd>"));
-/*
-    var divscope = function (itemprop) {
-        var scrie = '<div itemprop=\"' + itemprop + '\" itemscope '
-            + 'itemtype=\"http://schema.org/' + udata[itemprop] + '\"></div>';
-        if (console.log) {
-            console.log('divscope scrie = ' + scrie);
-        }
-        return scrie;
-    };
-  */
-    /*
-    var spanprop = function (itemprop,itemval) {
-        var scrie = '<span itemprop=\"' + itemprop + '\">' + itemval + '</span>';
-        if (console.log) {
-            console.log('spanprop scrie = ' + scrie);
-        }
-        return scrie;
-    };
-    */
+
 
     $("#makeaut").click(function() {
         //input dupa id
@@ -182,39 +155,53 @@ $(document).ready(function () {
            console.log('Clicked X - delclicks = ' + theclicks.getClicks() + ' vegades!');
        }
     });
+
     form.on("change","input",function () {
         var itemtype = this.className;       //ok
         var itemprop = this.name;                //ok
         var itemval = this.value;                    //ok
-
             if (console.log) {
                 console.log('itemtype = ' + itemtype);
                 console.log('itemprop = ' + itemprop);
                 console.log('itemval = ' + itemval);
                 //console.log('meta.itemtype.itemprop = ' + meta.itemtype.itemprop);
             }
-
-        //divdata.append(spanprop(itemprop,itemval));
-
-/*            var dl = $("#properties");
-            var dt = dl.append($('<dt>' + itemprop + '</dt>'));
-            //var dt = $("#" + itemprop);
-            var dd = dt.next();
-            if (dd.is("dd")) {
-                dd.remove();
-            }
-            dt.append($('<dd itemprop=\"' + itemprop + '\">' + itemval + '</dd>'));
-            //   var a = obtenir(udate.uid);
- */
     });
-
-    /*
-     $("#buton4").click(function () {
-
-     });
-     */
-    /*
-     *   <input id = "wabout" name = "wabout" type = "text" value = ""/>
-     *   <span id = "about" itemprop = "about" content = "About"></span>
-     */
 });
+
+/*
+ //divdata.append(spanprop(itemprop,itemval));
+ var dl = $("#properties");
+ var dt = dl.append($('<dt>' + itemprop + '</dt>'));
+ //var dt = $("#" + itemprop);
+ var dd = dt.next();
+ if (dd.is("dd")) {
+ dd.remove();
+ }
+ dt.append($('<dd itemprop=\"' + itemprop + '\">' + itemval + '</dd>'));
+ //   var a = obtenir(udate.uid);
+ // dt.append($("<dd itemprop=\"" + itemprop + "\">" + print + "</dd>"));
+ var divscope = function (itemprop) {
+ var scrie = '<div itemprop=\"' + itemprop + '\" itemscope '
+ + 'itemtype=\"http://schema.org/' + udata[itemprop] + '\"></div>';
+ if (console.log) {
+ console.log('divscope scrie = ' + scrie);
+ }
+ return scrie;
+ };
+
+
+ var spanprop = function (itemprop,itemval) {
+ var scrie = '<span itemprop=\"' + itemprop + '\">' + itemval + '</span>';
+ if (console.log) {
+ console.log('spanprop scrie = ' + scrie);
+ }
+ return scrie;
+ };
+
+ $("#buton4").click(function () {
+
+ });
+ *   <input id = "wabout" name = "wabout" type = "text" value = ""/>
+ *   <span id = "about" itemprop = "about" content = "About"></span>
+ */
