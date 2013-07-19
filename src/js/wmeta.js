5e7ecebf352fa9357568e8bc5fbbad1e1ca91b96
Diana Kramer <diana@warelet.com>
2013-07-19 13:52:23 +0200

    The tree of schemas.

--- src/js/wmeta.js
+++ src/js/wmeta.js
@@ -6,16 +6,10 @@ function printScope(itemtype) {
 }
 
 var Itemprop = function () {
-    this.itemtag = string;
     this.itemname = string;
     this.itemclass = string;
-    this.itemtype = string;
     this.itemvalue = string;
     this.itemparents = string;
-};
-
-Itemprop.prototype.get_itemtag = function () {
-    return this.itemtag;
 };
 
 Itemprop.prototype.get_itemname = function () {
@@ -24,10 +18,6 @@ Itemprop.prototype.get_itemname = function () {
 
 Itemprop.prototype.get_itemclass = function () {
     return this.itemclass;
-};
-
-Itemprop.prototype.get_itemtype = function () {
-    return this.itemtype;
 };
 
 Itemprop.prototype.get_itemvalue = function () {
@@ -40,40 +30,47 @@ Itemprop.prototype.get_itemparents = function () {
 
 var nodePrint = {};
 var index = 0;
+
 function printDom(param) {
     var indexx = 0;
-    ++index;
     var print = $(param).children();
     $(print).each(function (i) {
-        //var mytag = '<div>______</div>';
-        //var mytag = mytag + '<div>__' + index + '__' + indexx + '</div>';
-        //var mytag = mytag + '<div>______</div>';
-        //mytag = mytag + " Print: " + $(print[i]).prop("tagName") + ' i = ' + i;
         if ($(print[i]).prop("tagName") === "INPUT") {
-            //if (print[i].type !== "radio") {
-            var scrie = '<div>_____</div>';
-            scrie = scrie + '<div> _tag:' + $(print[i]).prop("tagName") + '</div>';
-            scrie = scrie + '<div> _name:' + print[i].name + '</div>';
-            scrie = scrie + '<div> _class:' + print[i].className + '</div>';
-            scrie = scrie + '<div> _type:' + print[i].type + '</div>';
-            scrie = scrie + '<div> _value:' + print[i].value + '</div>';
-            var the_parents = $(print[i]).parents("fieldset").first()
-                .map(function () {
-                    return this.className;
-                })
-                .get().join(", ");
-            scrie = scrie + '<div> _parents:' + the_parents + '</div>';
-            $("#buttonprint").append(scrie);
-            //}
-            //nodePrint[$(print[i]).prop("tagName")] = print[i].name;
+            if (print[i].type !== "radio") {
+                var scrie = '<div>_____</div>';
+                var scrie = scrie + '<div>__index: ' + index + '__</div>';
+                scrie = scrie + '<div> _itemprop: ' + print[i].name + '</div>';
+                scrie = scrie + '<div> _itemtype: ' + print[i].className + '</div>';
+                scrie = scrie + '<div> _value: ' + print[i].value + '</div>';
+                var itemlabel;
+                if ($($(print[i]).parent().siblings("legend")).text() != print[i].className) {
+                    itemlabel = $($(print[i]).parent().siblings("legend")).text();
+                } else {
+                    itemlabel = "";
+                }
+                scrie = scrie + '<div> _itemschema: ' + itemlabel + '</div>';
+                var j = 0;
+                do {
+                    var all_parents = $(print[i]).parents("fieldset").eq(j)
+                        .map(function () {
+                            return this.className;
+                        })
+                        .get()
+                        .join(" ");
+                    if ((itemlabel != "") && (all_parents != "") && (all_parents !== itemlabel)) {
+                        scrie = scrie + '<div> _' + j + ' parent: ' + all_parents + '</div>';
+                        ++indexx;
+                    }
+                    j++;
+                } while (all_parents != "");
+                var scrie = scrie + '<div>__indexx: ' + indexx + '__</div>';
+                $("#buttonprint").append(scrie);
+            }
+            ++index;
         }
-        ++indexx;
-        //$("#buttonprint").append(mytag);
         printDom(print[i]);
     });
     consoleVect(nodePrint, "nodePrint");
-    //var scrie = printObj(nodePrint);
-    //$("#buttonprint").append(scrie);
 }
 
 function consoleVect(param, nume) {
